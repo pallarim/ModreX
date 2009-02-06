0a6f428a825119726ab7900f7df2c6b079bd7ea3
mikkopa <mikkopa@8aa403fa-3bed-44d9-bcf7-2e286cdfca1b>
2009-02-06 13:35:41 +0000

    Added the file that was missing from the last commit

---
+++ ModularRex/RexNetwork/IClientMediaURL.cs
@@ -0,0 +1,13 @@
+﻿using OpenMetaverse;
+
+namespace ModularRex.RexNetwork
+{
+    /// <summary>
+    /// This client supports Rex style MediaURLs
+    /// </summary>
+    public interface IClientMediaURL
+    {
+        event ReceiveRexMediaURL OnReceiveRexMediaURL;
+        void SendMediaURL(UUID assetId, string mediaURL, byte refreshRate);
+    }
+}
