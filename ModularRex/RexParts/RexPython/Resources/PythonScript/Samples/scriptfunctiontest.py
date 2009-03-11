28aebb129a7d0ccfd2bd6e717e9acd7f801542ff
mikkopa <mikkopa@8aa403fa-3bed-44d9-bcf7-2e286cdfca1b>
2009-03-11 10:36:22 +0000

    Added new tests to test more of the API functionality.

--- ModularRex/RexParts/RexPython/Resources/PythonScript/Samples/scriptfunctiontest.py
+++ ModularRex/RexParts/RexPython/Resources/PythonScript/Samples/scriptfunctiontest.py
@@ -17,12 +17,14 @@ class Test(rxactor.Actor):
     def EventCreated(self):
         super(self.__class__,self).EventCreated()
         self.TickCount = 0
-        self.MyTimer = self.CreateRexTimer(4,10)
+        self.MyTimer = self.CreateRexTimer(4,19)
         self.MyTimer.onTimer += self.HandleTimer
         
         #change these values to be suitable in your test environment
-        self.__meshId = "82bfa22b-02b6-b0aa-71fb-799b29e061dc"
+        self.__meshId = "50dae2f8-e1c9-7d28-2ff7-0cad68c9db97"  #jack mesh
         self.__particleScriptId = "676e66ce-c788-1b5a-5bef-50477f13c60a"
+        self.__animationId = "05aaa369-d602-afec-61a6-407a6cdbbd9f"
+        self.__animationName = "Wave"
 
         print "scriptfunctiontest.Test EventCreated"
 
@@ -111,3 +113,61 @@ class Test(rxactor.Actor):
             self.llSay(0, "test 9 & 10 passed")
         else:
             self.llSay(0, "test failed, asset "+vAssetId+" found")
+    
+    #SetRexAnimationPackageUUID
+    def Test11(self):
+        self.SetRexAnimationPackageUUID(self.__animationId)
+        
+    #GetRexAnimationPackageUUID 
+    def Test12(self):
+        vAssetId = self.GetRexAnimationPackageUUID()
+        if vAssetId == self.__animationId :
+            self.llSay(0, "test 11 & 12 passed")
+        else:
+            self.llSay(0, "test failed, asset "+vAssetId+" found")
+
+    #SetRexAnimationName
+    def Test13(self):
+        self.SetRexAnimationName(self.__animationName)
+
+    #GetRexAnimationName 
+    def Test14(self):
+        vAssetId = self.GetRexAnimationName()
+        if vAssetId == self.__animationName :
+            self.llSay(0, "test 13 & 14 passed")
+        else:
+            self.llSay(0, "test failed, asset "+vAssetId+" found")
+
+    #SetRexAnimationRate
+    def Test15(self):
+        self.SetRexAnimationRate(2)
+
+    #GetRexAnimationRate
+    def Test16(self):
+        vRate = self.GetRexAnimationRate()
+        if vRate == 2 :
+            self.llSay(0, "test 15 & 16 passed")
+        else:
+            self.llSay(0, "test failed, asset "+vAssetId+" found")
+
+    #RexGetMaterialCount
+    def Test17(self):
+        self.__materialCount = self.RexGetMaterialCount()
+        if self.__materialCount == 2:
+            self.llSay(0, "test 17 passed")
+        else:
+            self.llSay(0, "test RexGetMaterialCount failed. either wrong mesh is used or setting mesh also failed.")
+            self.llSay(0, "Number of materials: "+self.__materialCount)
+
+    #RexSetMaterialUUID
+    def Test18(self):
+        if self.__materialCount >= 1:
+            self.RexSetMaterialUUID(0, "00000000-0000-2222-3333-100000001013") #this asset is in OpenSim library
+
+    #RexGetMaterial
+    def Test19(self):
+        vAsset = self.RexGetMaterial(0)
+        if vAsset == "00000000-0000-2222-3333-100000001013":
+            self.llSay(0, "test 18 & 19 passed")
+        else:
+            self.llSay(0, "test failed, asset "+vAsset+" found")
