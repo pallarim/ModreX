479f465aa0b06810d39a6a949afd685b6fc47dd8
mikkopa <mikkopa@8aa403fa-3bed-44d9-bcf7-2e286cdfca1b>
2009-03-12 11:35:11 +0000

    Added new tests to test functionality of python script engine.

--- ModularRex/RexParts/RexPython/Resources/PythonScript/Samples/scriptfunctiontest.py
+++ ModularRex/RexParts/RexPython/Resources/PythonScript/Samples/scriptfunctiontest.py
@@ -15,16 +15,19 @@ class Test(rxactor.Actor):
         return "scriptfunctiontest.Test"
 
     def EventCreated(self):
-        super(self.__class__,self).EventCreated()
-        self.TickCount = 0
-        self.MyTimer = self.CreateRexTimer(4,19)
-        self.MyTimer.onTimer += self.HandleTimer
-        
         #change these values to be suitable in your test environment
         self.__meshId = "50dae2f8-e1c9-7d28-2ff7-0cad68c9db97"  #jack mesh
         self.__particleScriptId = "676e66ce-c788-1b5a-5bef-50477f13c60a"
         self.__animationId = "05aaa369-d602-afec-61a6-407a6cdbbd9f"
         self.__animationName = "Wave"
+
+        super(self.__class__,self).EventCreated()
+        self.TickCount = 0
+        self.MyTimer = self.CreateRexTimer(4,33)
+        self.MyTimer.onTimer += self.HandleTimer
+        
+        self._testsRun = 0
+        self._testsOK = 0
 
         print "scriptfunctiontest.Test EventCreated"
 
@@ -40,6 +43,8 @@ class Test(rxactor.Actor):
             self.llShout(0,"Test already running...")
             return
 
+        self._testsRun = 0
+        self._testsOK = 0
         self.llShout(0,"Starting test")
         self.CurrentTest = 0
         self.AgentId = vAvatar.AgentId
@@ -53,6 +58,8 @@ class Test(rxactor.Actor):
     def HandleTimer(self):
         self.CurrentTest = self.CurrentTest+1
         eval("self.Test"+str(self.CurrentTest)+"()")
+        self._testsRun += 1
+        print "Tests passed: "+ str(self._testsOK) +"/"+ str(self._testsRun)
 
     #SetRexDrawDistance
     def Test1(self):
@@ -65,6 +72,7 @@ class Test(rxactor.Actor):
             self.llSay(0, "SetRexDrawDistance - GetRexDrawDistance test failed")
         else:
             self.llSay(0, "test 1 & 2 passed")
+            self._testsOK += 2
 
     #SetRexLOD
     def Test3(self):
@@ -77,6 +85,7 @@ class Test(rxactor.Actor):
             self.llSay(0, "SetRexLOD - GetRexLOD test failed")
         else :
             self.llSay(0, "test 3 & 4 passed")
+            self._testsOK += 2
 
     #SetRexMeshUUID
     def Test5(self):
@@ -87,6 +96,7 @@ class Test(rxactor.Actor):
         vAssetId = self.GetRexMeshUUID()
         if vAssetId == self.__meshId:
             self.llSay(0, "test 5 & 6 passed")
+            self._testsOK += 2
         else:
             self.llSay(0, "test failed, asset "+vAssetId+" found")
 
@@ -99,6 +109,7 @@ class Test(rxactor.Actor):
         vAssetId = self.GetRexCollisionMeshUUID()
         if vAssetId == self.__meshId:
             self.llSay(0, "test 7 & 8 passed")
+            self._testsOK += 2
         else:
             self.llSay(0, "test failed, asset "+vAssetId+" found")
 
@@ -111,6 +122,7 @@ class Test(rxactor.Actor):
         vAssetId = self.GetRexParticleScriptUUID()
         if vAssetId == self.__particleScriptId:
             self.llSay(0, "test 9 & 10 passed")
+            self._testsOK += 2
         else:
             self.llSay(0, "test failed, asset "+vAssetId+" found")
     
@@ -123,6 +135,7 @@ class Test(rxactor.Actor):
         vAssetId = self.GetRexAnimationPackageUUID()
         if vAssetId == self.__animationId :
             self.llSay(0, "test 11 & 12 passed")
+            self._testsOK += 2
         else:
             self.llSay(0, "test failed, asset "+vAssetId+" found")
 
@@ -135,6 +148,7 @@ class Test(rxactor.Actor):
         vAssetId = self.GetRexAnimationName()
         if vAssetId == self.__animationName :
             self.llSay(0, "test 13 & 14 passed")
+            self._testsOK += 2
         else:
             self.llSay(0, "test failed, asset "+vAssetId+" found")
 
@@ -147,6 +161,7 @@ class Test(rxactor.Actor):
         vRate = self.GetRexAnimationRate()
         if vRate == 2 :
             self.llSay(0, "test 15 & 16 passed")
+            self._testsOK += 2
         else:
             self.llSay(0, "test failed, asset "+vAssetId+" found")
 
@@ -155,6 +170,7 @@ class Test(rxactor.Actor):
         self.__materialCount = self.RexGetMaterialCount()
         if self.__materialCount == 2:
             self.llSay(0, "test 17 passed")
+            self._testsOK += 1
         else:
             self.llSay(0, "test RexGetMaterialCount failed. either wrong mesh is used or setting mesh also failed.")
             self.llSay(0, "Number of materials: "+self.__materialCount)
@@ -169,5 +185,106 @@ class Test(rxactor.Actor):
         vAsset = self.RexGetMaterial(0)
         if vAsset == "00000000-0000-2222-3333-100000001013":
             self.llSay(0, "test 18 & 19 passed")
+            self._testsOK += 2
         else:
             self.llSay(0, "test failed, asset "+vAsset+" found")
+            
+    #SetRexSoundVolume
+    def Test20(self):
+        self.SetRexSoundVolume(3.0)
+    #GetRexSoundVolume
+    def Test21(self):
+        soundVol = self.GetRexSoundVolume()
+        if soundVol == 3.0:
+            self.llSay(0, "test 20 & 21 passed")
+            self._testsOK += 2
+        else:
+            self.llSay(0, "test 20 & 21 failed")
+
+    #SetRexSoundRadius
+    def Test22(self):
+        self.SetRexSoundRadius(3.0)
+    #GetRexSoundRadius
+    def Test23(self):
+        soundRad = self.GetRexSoundRadius()
+        if soundRad == 3.0:
+            self.llSay(0, "test 22 & 23 passed")
+            self._testsOK += 2
+        else:
+            self.llSay(0, "test 22 & 23 failed")
+            
+    #SetRexSelectPriority
+    def Test24(self):
+        self.SetRexSelectPriority(3.0)
+    #GetRexSelectPriority
+    def Test25(self):
+        sPrio = self.GetRexSelectPriority()
+        if sPrio == 3.0:
+            self.llSay(0, "test 24 & 25 passed")
+            self._testsOK += 2
+        else:
+            self.llSay(0, "test 24 & 25 failed")
+
+    #SetVelocity
+    def Test26(self):
+        tVel = Vector3(0,0,2)
+        self.SetVelocity(tVel)
+        vVel = self.llGetVel()
+
+        if tVel == vVel:
+            self.llSay(0, "test 26 passed")
+            self._testsOK += 1
+        else :
+            self.llSay(0, "test 26 failed. vVel="+vVel.ToString())
+
+    #SetPhysics
+    def Test27(self):
+        self.SetVelocity(Vector3(0,0,0))
+        self.SetPhysics(False)
+        
+    #GetPhysics
+    def Test28(self):
+        vbPhys = self.GetPhysics()
+        if vbPhys == False :
+            self.llSay(0, "test 27 & 28 passed")
+            self._testsOK += 2
+        else :
+            self.llSay(0, "test 27 & 28 failed")
+            
+    #SendAlertToAvatar
+    def Test29(self):
+        self.SendAlertToAvatar(self.AgentId, "Test 29 passed", True)
+        self._testsOK += 1
+        
+    #CommandToClient
+    def Test30(self):
+        self.CommandToClient(self.AgentId, 'hud', 'ShowScrollMessage("Test30 passed", 30)', "")
+        self._testsOK += 1
+
+    #GetPrimLocalIdFromUUID
+    def Test31(self):
+        myUUID = self.llGetKey().ToString()
+        vId = self.GetPrimLocalIdFromUUID(myUUID)
+        if str(self.Id) == str(vId):
+            self.llSay(0, "test 31 passed")
+            self._testsOK += 1
+        else :
+            self.llSay(0, "test 31 failed. vId="+str(vId)+" Id="+str(self.Id))
+
+    #GetRadiusActors
+    def Test32(self):
+        actors = self.GetRadiusActors(100)
+        if len(actors) >= 2:
+            self.llSay(0, "test 32 passed")
+            self._testsOK += 1
+        else :
+            self.llSay(0, "test 32 failed")
+
+    #GetRadiusAvatars
+    def Test33(self):
+        avatars = self.GetRadiusAvatars(100)
+        if len(avatars) >= 1:
+            self.llSay(0, "test 33 passed")
+            self._testsOK += 1
+        else :
+            self.llSay(0, "test 33 failed")
