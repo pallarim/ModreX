8ba7c271bf674c6785aadd2ad985ebe689ab0b38
mikkopa <mikkopa@8aa403fa-3bed-44d9-bcf7-2e286cdfca1b>
2009-05-11 10:54:14 +0000

    Added the AsyncSceneObjectGroupDeleter functions to rex object de-rez

--- ModularRex/RexParts/RexObjRez.cs
+++ ModularRex/RexParts/RexObjRez.cs
@@ -9,6 +9,7 @@ using log4net;
 using System.Reflection;
 using OpenSim.Framework.Communications.Cache;
 using ModularRex.RexFramework;
+using System.Timers;
 
 namespace ModularRex.RexParts
 {
@@ -18,6 +19,14 @@ namespace ModularRex.RexParts
 
         private Scene m_scene;
         private ModrexObjects m_rexObjects;
+
+        /// <value>
+        /// Is the deleter currently enabled?
+        /// </value>
+        public bool DeleterEnabled;
+
+        private Timer m_inventoryTicker = new Timer(2000);
+        private readonly Queue<DeleteToInventoryHolder> m_inventoryDeletes = new Queue<DeleteToInventoryHolder>();
 
         #region IRegionModule Members
 
@@ -42,6 +51,9 @@ namespace ModularRex.RexParts
 
         public void PostInitialise()
         {
+            m_inventoryTicker.AutoReset = false;
+            m_inventoryTicker.Elapsed += InventoryRunDeleteTimer;
+
             m_scene.EventManager.OnNewClient += HandleNewClient;
 
             OpenSim.Region.Framework.Interfaces.IRegionModule module = m_scene.Modules["RexObjectsModule"];
@@ -49,7 +61,12 @@ namespace ModularRex.RexParts
             {
                 m_rexObjects = (ModrexObjects)module;
             }
+            DeleterEnabled = true;
         }
+
+        #endregion
+
+        #region Event Handlers
 
         private void HandleNewClient(IClientAPI client)
         {
@@ -158,33 +175,35 @@ namespace ModularRex.RexParts
 
             if (permissionToTake)
             {
-                //TODO: Change this to some kind of async queue
-                //The queue was orginally introduced in OpenSim revision 5346:
-                // * Moves sending items to inventory via a delete into a seperate thread (this thread
-                //   can be expanded to support all sends to inventory from inworld easily enough). 
-                //   Thread is temporary and only exists while items are being returned.
-                // * This should remove the "lag" caused by deleting many objects.
-                // * Patch brought to you by Joshua Nightshade's bitching at me to fix it.
-                UUID assetId = m_scene.DeleteToInventory(action, destinationID, grp, remoteClient);
+
+                DeleteToInventory(action, destinationID, grp, remoteClient, permissionToDelete);
+
+                ////TODO: Change this to some kind of async queue
+                ////The queue was orginally introduced in OpenSim revision 5346:
+                //// * Moves sending items to inventory via a delete into a seperate thread (this thread
+                ////   can be expanded to support all sends to inventory from inworld easily enough). 
+                ////   Thread is temporary and only exists while items are being returned.
+                //// * This should remove the "lag" caused by deleting many objects.
+                //// * Patch brought to you by Joshua Nightshade's bitching at me to fix it.
+                //UUID assetId = m_scene.DeleteToInventory(action, destinationID, grp, remoteClient);
  
-                //Get the item id of the asset so the RexObjectProperties can be changed to that id
-                CachedUserInfo userInfo = m_scene.CommsManager.UserProfileCacheService.GetUserDetails(remoteClient.AgentId);
-                InventoryItemBase item = userInfo.RootFolder.FindAsset(assetId);
+                ////Get the item id of the asset so the RexObjectProperties can be changed to that id
+                //CachedUserInfo userInfo = m_scene.CommsManager.UserProfileCacheService.GetUserDetails(remoteClient.AgentId);
+                //InventoryItemBase item = userInfo.RootFolder.FindAsset(assetId);
 
-                //Clone the old properties 
-                if (m_rexObjects != null)
-                {
-                    RexObjectProperties origprops = m_rexObjects.GetObject(part.UUID);
-                    RexObjectProperties cloneprops = m_rexObjects.GetObject(item.ID);
+                ////Clone the old properties 
+                //if (m_rexObjects != null)
+                //{
+                //    RexObjectProperties origprops = m_rexObjects.GetObject(part.UUID);
+                //    RexObjectProperties cloneprops = m_rexObjects.GetObject(item.ID);
 
-                    cloneprops.SetRexPrimDataFromObject(origprops);
-                }
+                //    cloneprops.SetRexPrimDataFromObject(origprops);
+                //}
 
-                if (permissionToDelete)
-                {
-                    grp.DeleteGroup(false);
-                    m_rexObjects.DeleteObject(part.UUID);
-                }
+                //if (permissionToDelete)
+                //{
+                //    grp.DeleteGroup(false);
+                //}
 
             }
             else if (permissionToDelete)
@@ -205,5 +224,130 @@ namespace ModularRex.RexParts
         }
 
         #endregion
+
+        #region AsyncSceneObjectGroupDeleter
+
+        /// <summary>
+        /// Delete the given object from the scene
+        /// </summary>
+        public void DeleteToInventory(DeRezAction action, UUID folderID,
+                SceneObjectGroup objectGroup, IClientAPI remoteClient,
+                bool permissionToDelete)
+        {
+            if (DeleterEnabled)
+                m_inventoryTicker.Stop();
+
+            lock (m_inventoryDeletes)
+            {
+                DeleteToInventoryHolder dtis = new DeleteToInventoryHolder();
+                dtis.action = action;
+                dtis.folderID = folderID;
+                dtis.objectGroup = objectGroup;
+                dtis.remoteClient = remoteClient;
+                dtis.permissionToDelete = permissionToDelete;
+
+                m_inventoryDeletes.Enqueue(dtis);
+            }
+
+            if (DeleterEnabled)
+                m_inventoryTicker.Start();
+
+            // Visually remove it, even if it isnt really gone yet.  This means that if we crash before the object
+            // has gone to inventory, it will reappear in the region again on restart instead of being lost.
+            // This is not ideal since the object will still be available for manipulation when it should be, but it's
+            // better than losing the object for now.
+            if (permissionToDelete)
+                objectGroup.DeleteGroup(false);
+        }
+
+        private void InventoryRunDeleteTimer(object sender, ElapsedEventArgs e)
+        {
+            m_log.Debug("[SCENE]: Starting send to inventory loop");
+
+            while (InventoryDeQueueAndDelete())
+            {
+                m_log.Debug("[SCENE]: Sent item successfully to inventory, continuing...");
+            }
+        }
+
+        /// <summary>
+        /// Move the next object in the queue to inventory.  Then delete it properly from the scene.
+        /// </summary>
+        /// <returns></returns>
+        public bool InventoryDeQueueAndDelete()
+        {
+            DeleteToInventoryHolder x = null;
+
+            try
+            {
+                lock (m_inventoryDeletes)
+                {
+                    int left = m_inventoryDeletes.Count;
+                    if (left > 0)
+                    {
+                        m_log.DebugFormat(
+                            "[SCENE]: Sending object to user's inventory, {0} item(s) remaining.", left);
+
+                        x = m_inventoryDeletes.Dequeue();
+
+                        try
+                        {
+                            //m_scene.DeleteToInventory(x.action, x.folderID, x.objectGroup, x.remoteClient);
+                            //if (x.permissionToDelete)
+                            //    m_scene.DeleteSceneObject(x.objectGroup, false);
+
+                            UUID assetId = m_scene.DeleteToInventory(x.action, x.folderID, x.objectGroup, x.remoteClient);
+
+                            //Get the item id of the asset so the RexObjectProperties can be changed to that id
+                            CachedUserInfo userInfo = m_scene.CommsManager.UserProfileCacheService.GetUserDetails(x.remoteClient.AgentId);
+                            InventoryItemBase item = userInfo.RootFolder.FindAsset(assetId);
+
+                            //Clone the old properties 
+                            if (m_rexObjects != null)
+                            {
+                                RexObjectProperties origprops = m_rexObjects.GetObject(x.objectGroup.RootPart.UUID);
+                                RexObjectProperties cloneprops = m_rexObjects.GetObject(item.ID);
+
+                                cloneprops.SetRexPrimDataFromObject(origprops);
+                            }
+
+                            if (x.permissionToDelete)
+                            {
+                                m_scene.DeleteSceneObject(x.objectGroup, false);
+                                m_rexObjects.DeleteObject(x.objectGroup.RootPart.UUID);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            m_log.DebugFormat("Exception background sending object: " + e);
+                        }
+
+                        return true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // We can't put the object group details in here since the root part may have disappeared (which is where these sit).
+                // FIXME: This needs to be fixed.
+                m_log.ErrorFormat(
+                    "[SCENE]: Queued sending of scene object to agent {0} {1} failed: {2}",
+                    (x != null ? x.remoteClient.Name : "unavailable"), (x != null ? x.remoteClient.AgentId.ToString() : "unavailable"), e.ToString());
+            }
+
+            m_log.Debug("[SCENE]: No objects left in inventory send queue.");
+            return false;
+        }
+
+        #endregion
+    }
+
+    class DeleteToInventoryHolder
+    {
+        public DeRezAction action;
+        public IClientAPI remoteClient;
+        public SceneObjectGroup objectGroup;
+        public UUID folderID;
+        public bool permissionToDelete;
     }
 }
