34c16255423a3139a330aa0734353a0888afcf6b
sempuki <sempuki@8aa403fa-3bed-44d9-bcf7-2e286cdfca1b>
2009-06-22 11:38:51 +0000

    spruced up the linked build step

--- ModularRex/runprebuild-linked.sh
+++ ModularRex/runprebuild-linked.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sed 's/\.\.\/\.\.\/bin/OpenSim\/bin/' prebuild.xml > linked-prebuild.xml
-mono ./OpenSim/bin/Prebuild.exe /file linked-prebuild.xml /target nant
-mono ./OpenSim/bin/Prebuild.exe /file linked-prebuild.xml /target monodev
+#sed 's/\.\.\/\.\.\/bin/OpenSim\/bin/' prebuild.xml > linked-prebuild.xml
+cp prebuild.xml rebased-prebuild.xml
+./rebase-prebuild-path.sh rebased-prebuild.xml OutputPath "../../bin" "OpenSim/bin"
+./rebase-prebuild-path.sh rebased-prebuild.xml ReferencePath "../../bin" "OpenSim/bin"
+mono ./OpenSim/bin/Prebuild.exe /file rebased-prebuild.xml /target nant
+mono ./OpenSim/bin/Prebuild.exe /file rebased-prebuild.xml /target monodev
 cp ModularRex.build default.build
