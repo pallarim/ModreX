891e94811defcbb76c6b5c66a66940fe472ea282
mikkopa <mikkopa@8aa403fa-3bed-44d9-bcf7-2e286cdfca1b>
2009-09-11 12:14:43 +0000

    Added a new client view: RexClientViewCompatible. This client view is to be used in worlds and/or client stacks that are compatible with both LL and realXtend clients. Main difference with RexClientViewBase for now is that with this client view all the avatar updates are SL style avatar updates, not compressed ones like with realXtend clients.

---
+++ ModularRex/RexNetwork/ClientViews/RexClientViewCompatible.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+using OpenMetaverse.Packets;
+using System.Net;
+using OpenSim.Framework;
+using OpenSim.Region.ClientStack.LindenUDP;
+using OpenSim.Region.ClientStack;
+
+namespace ModularRex.RexNetwork
+{
+    /// <summary>
+    /// This client view is ment for realXtend clients and it is compatible with LLClient.
+    /// This client view should not crash LL clients or their derivants.
+    /// </summary>
+    public class RexClientViewCompatible : RexClientViewBase
+    {
+        public RexClientViewCompatible(EndPoint remoteEP, IScene scene,
+                             LLPacketServer packServer, AuthenticateResponse authenSessions, UUID agentId,
+                             UUID sessionId, uint circuitCode, EndPoint proxyEP, ClientStackUserSettings userSettings)
+            : base(remoteEP, scene, packServer, authenSessions, agentId,
+                   sessionId, circuitCode, proxyEP, userSettings)
+        {
+        }
+
+        public override void SendAvatarTerseUpdate(ulong regionHandle,
+                ushort timeDilation, uint localID, Vector3 position,
+                Vector3 velocity, Quaternion rotation, UUID agentid)
+        {
+            if (rotation.X == rotation.Y &&
+                rotation.Y == rotation.Z &&
+                rotation.Z == rotation.W && rotation.W == 0)
+                rotation = Quaternion.Identity;
+
+            position.Z = (float)(position.Z - 0.15);
+
+            ImprovedTerseObjectUpdatePacket.ObjectDataBlock terseBlock =
+                CreateAvatarImprovedBlock(localID, position, velocity, rotation);
+
+            lock (m_avatarTerseUpdates)
+            {
+                m_avatarTerseUpdates.Add(terseBlock);
+
+                // If packet is full or own movement packet, send it.
+                if (m_avatarTerseUpdates.Count >= m_avatarTerseUpdatesPerPacket)
+                {
+                    ProcessAvatarTerseUpdates(this, null);
+                }
+                else if (m_avatarTerseUpdates.Count == 1)
+                {
+                    m_avatarTerseUpdateTimer.Start();
+                }
+            }
+        }
+    }
+}
