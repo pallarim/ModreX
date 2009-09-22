eb86bfbcc6336ba84b64fcfda8507f38d278e732
mikkopa <mikkopa@8aa403fa-3bed-44d9-bcf7-2e286cdfca1b>
2009-09-22 08:47:51 +0000

    * Added WebDAVSharp project to prebuild.xml. This project is in svn-externals in directory lib * Added missing references to WorldInventory project in prebuild.xml * Renamed WorldInventory class to WorldInventoryModule * Added new class WorldInventoryServer. This is the class that does all the work when making WebDAV requests to WorldInventory * Added HttpServer.dll that is referenced by WebDAVSharp. Note: this build may not work for others since it is build with Win64.

---
+++ ModularRex/WorldInventory/WorldInventoryModule.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenSim.Framework;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Region.Framework.Scenes;
+using Nini.Config;
+using HttpServer;
+using log4net;
+using System.Reflection;
+using System.Net;
+using WebDAVSharp;
+
+namespace ModularRex.WorldInventory
+{
+    public class WorldInventoryModule : IRegionModule
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        #region IRegionModule Members
+
+        protected WorldInventoryServer m_server = null;
+
+        private List<Scene> m_scenes = new List<Scene>();
+        private int m_port = 6000;
+        private bool enabled = false;
+
+        public void Initialise(Scene scene, IConfigSource source)
+        {
+            m_scenes.Add(scene);
+            IConfig config = source.Configs["realXtend"];
+            if (config != null)
+            {
+                enabled = config.GetBoolean("WorldInventoryOn", false);
+                m_port = config.GetInt("WorldInventoryPort", 6000);
+            }
+        }
+
+        public void PostInitialise()
+        {
+            if (enabled)
+            {
+                IPAddress ip = m_scenes[0].RegionInfo.ExternalEndPoint.Address;
+                m_server = new WorldInventoryServer(m_scenes);
+                m_server.Start(ip, m_port);
+            }
+        }
+
+        public void Close()
+        {
+            if (m_server != null)
+                m_server.Stop();
+        }
+
+        public string Name
+        {
+            get { return "WorldInventory"; }
+        }
+
+        public bool IsSharedModule
+        {
+            get { return true; }
+        }
+
+        #endregion
+    }
+
+    public class HttpServerLogWriter : ILogWriter
+    {
+        //private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void Write(object source, LogPrio priority, string message)
+        {
+            /*
+            switch (priority)
+            {
+                case HttpServer.LogPrio.Debug:
+                    m_log.DebugFormat("[{0}]: {1}", source.ToString(), message);
+                    break;
+                case HttpServer.LogPrio.Error:
+                    m_log.ErrorFormat("[{0}]: {1}", source.ToString(), message);
+                    break;
+                case HttpServer.LogPrio.Info:
+                    m_log.InfoFormat("[{0}]: {1}", source.ToString(), message);
+                    break;
+                case HttpServer.LogPrio.Warning:
+                    m_log.WarnFormat("[{0}]: {1}", source.ToString(), message);
+                    break;
+                case HttpServer.LogPrio.Fatal:
+                    m_log.ErrorFormat("[{0}]: FATAL! - {1}", source.ToString(), message);
+                    break;
+                default:
+                    break;
+            }
+            */
+
+            return;
+        }
+    }
+}
---
+++ ModularRex/WorldInventory/WorldInventoryServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HttpServer;
+using WebDAVSharp;
+using OpenSim.Region.Framework.Scenes;
+
+namespace ModularRex.WorldInventory
+{
+    public class WorldInventoryServer
+    {
+        private HttpServerLogWriter httpserverlog = new HttpServerLogWriter();
+
+        protected HttpListener m_listener = null;
+        protected WebDAVListener m_webdav = null;
+
+        private List<Scene> m_scenes = null;
+
+        public WorldInventoryServer()
+        {
+        }
+
+        public WorldInventoryServer(List<Scene> scenes)
+        {
+            m_scenes = scenes;
+        }
+
+        public void Start(System.Net.IPAddress ip, int port)
+        {
+            m_listener = HttpListener.Create(httpserverlog, ip, port);
+            m_webdav = new WebDAVListener(m_listener, @"^/inventory/");
+            m_listener.Start(10);
+
+            m_webdav.OnPropFind += PropFindHandler;
+        }
+
+        public void Stop()
+        {
+            m_webdav.OnPropFind -= PropFindHandler;
+            m_listener.Stop();
+        }
+
+        IList<IWebDAVResource> PropFindHandler(string username, string path, DepthHeader depth)
+        {
+            return null;
+        }
+    }
+}
