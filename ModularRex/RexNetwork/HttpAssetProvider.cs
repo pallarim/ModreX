a3008f8aa9871b6df303c0c682fe461546910278
mikkopa <mikkopa@8aa403fa-3bed-44d9-bcf7-2e286cdfca1b>
2010-03-19 11:33:26 +0000

    * Added module that can serve assets through http from simulator * Added module that can add asset url to all RexObjectProperties. Urls point to simulators listener.

---
+++ ModularRex/RexNetwork/HttpAssetProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenSim.Region.Framework.Interfaces;
+using OpenSim.Framework;
+using OpenSim.Services.Interfaces;
+using Nini.Config;
+using OpenSim.Region.Framework.Scenes;
+using OpenSim.Server.Handlers.Asset;
+using log4net;
+using System.Reflection;
+
+namespace ModularRex.RexNetwork
+{
+    public class HttpAssetProvider : IRegionModule
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private IAssetService m_AssetService;
+        private List<Scene> m_scenes = new List<Scene>();
+        private bool enabled = false;
+
+        public string Name
+        {
+            get { return "HttpAssetProvider"; }
+        }
+
+        #region IRegionModule Members
+
+        public void Close()
+        {
+        }
+
+        public void Initialise(Scene scene, IConfigSource source)
+        {
+            m_scenes.Add(scene);
+            if (source.Configs["realXtend"] != null)
+            {
+                enabled = !(source.Configs["realXtend"].GetBoolean("ServeHttpAssets", false));
+            }
+        }
+
+        public bool IsSharedModule
+        {
+            get { return true; }
+        }
+
+        public void PostInitialise()
+        {
+            if (enabled)
+            {
+                m_AssetService = m_scenes[0].AssetService;
+                if (m_AssetService != null)
+                    MainServer.Instance.AddStreamHandler(new AssetServerGetHandler(m_AssetService));
+                else
+                    m_log.Error("[HttpAssetProvider]: Could not initiate HttpAssetProvider since IAssetService is null!");
+            }
+        }
+
+        #endregion
+    }
+}
