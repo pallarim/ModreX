30ddaeac01f59b6e3f277db0fec87db93f564714
Mikko Pallari <pallarim@gmail.com>
2010-10-01 08:12:38 +0300

    Added new class, ECData, for storing Entity-Component data to database. Added also mapping for NHibernate and class for fetching data for it.

---
+++ ModularRex/NHibernate/NHibernateECData.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net;
+using System.Reflection;
+using ModularRex.RexFramework;
+using NHibernate;
+using OpenMetaverse;
+using NHibernate.Criterion;
+
+namespace ModularRex.NHibernate
+{
+    public class NHibernateECData
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public bool Inizialized = false;
+        private bool m_nullStorage = false;
+
+        public NHibernateManager manager;
+
+        public void Initialise(string connect)
+        {
+            m_log.InfoFormat("[NHIBERNATE] Initializing NHibernateECData");
+            if (connect.ToLower() == "null")
+            {
+                m_nullStorage = true;
+                Inizialized = true;
+                return;
+            }
+
+            Assembly assembly = GetType().Assembly;
+            manager = new NHibernateManager(connect, "ECData", assembly);
+            Inizialized = true;
+        }
+
+        public bool StoreComponent(ECData component)
+        {
+            if (m_nullStorage)
+                return false;
+
+            ICriteria criteria = manager.GetSession().CreateCriteria(typeof(ECData));
+            criteria.Add(Expression.Eq("EntityID", component.EntityID));
+            criteria.Add(Expression.Eq("ComponentType", component.ComponentName));
+            criteria.Add(Expression.Eq("ComponentName", component.ComponentName));
+
+            IList<ECData> result = criteria.List<ECData>();
+            if (result.Count == 0)
+            {
+                manager.Insert(component);
+            }
+            else if (result.Count == 1)
+            {
+                ECData data = result[0];
+                data.Data = component.Data;
+                data.DataIsString = component.DataIsString;
+                manager.Update(data);
+            }
+            else
+            {
+                throw new Exception("Could not store component, because component with same EntityID, ComponentType and ComponentName already exists in database");
+            }
+
+            return true;
+        }
+
+        public IList<ECData> GetComponents(UUID entityId)
+        {
+            if (m_nullStorage)
+                return new List<ECData>();
+
+            ICriteria criteria = manager.GetSession().CreateCriteria(typeof(ECData));
+            criteria.Add(Expression.Eq("EntityID", entityId));
+
+            return criteria.List<ECData>();
+        }
+    }
+}
---
+++ ModularRex/RexFramework/ECData.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenMetaverse;
+
+namespace ModularRex.RexFramework
+{
+    public class ECData
+    {
+        private UUID m_entity_id = UUID.Zero;
+        private string m_component_type = String.Empty;
+        private string m_component_name = String.Empty;
+        private byte[] m_data = null;
+        private bool m_data_is_string = false;
+
+        public ECData() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ECData"/> class.
+        /// </summary>
+        /// <param name="entityId">The entity id.</param>
+        /// <param name="componentName">Name of the component.</param>
+        /// <param name="data">The data of the component.</param>
+        /// <param name="dataIsString">if set to <c>true</c> <c>data</c> is string.</param>
+        public ECData(UUID entityId, string componentType, string componentName, byte[] data, bool dataIsString)
+        {
+            m_entity_id = entityId;
+            m_component_name = componentName;
+            m_data = data;
+            m_data_is_string = dataIsString;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ECData"/> class.
+        /// </summary>
+        /// <param name="entityId">The entity id.</param>
+        /// <param name="componentName">Name of the component.</param>
+        /// <param name="data">The data as base64 string.</param>
+        public ECData(UUID entityId, string componentType, string componentName, string data)
+        {
+            m_entity_id = entityId;
+            m_component_name = componentName;
+            m_data = Convert.FromBase64String(data);
+            m_data_is_string = true;
+        }
+
+        /// <summary>
+        /// Gets or sets the entity ID.
+        /// </summary>
+        /// <value>The entity ID to whom this component belongs to</value>
+        public virtual UUID EntityID
+        {
+            get { return m_entity_id; }
+            set { m_entity_id = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the component.
+        /// </summary>
+        /// <value>The type of the component.</value>
+        public virtual string ComponentType
+        {
+            get { return m_component_type; }
+            set { m_component_type = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the component.
+        /// </summary>
+        /// <value>The name of the component.</value>
+        public virtual string ComponentName
+        {
+            get { return m_component_name; }
+            set { m_component_name = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        /// <value>The data of the Component</value>
+        public virtual byte[] Data
+        {
+            get { return m_data; }
+            set { m_data = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether <c>Data</c> is string. If <c>Data</c> is string,
+        /// it should be convertable from byte array back to string with method <c>ToBase64String</c>
+        /// in <c>Convert</c> class.
+        /// </summary>
+        /// <value><c>true</c> if <c>Data</c> is string otherwise, <c>false</c>.</value>
+        public virtual bool DataIsString
+        {
+            get { return m_data_is_string; }
+            set { m_data_is_string = value; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            ECData t = obj as ECData;
+            if (t == null)
+                return false;
+
+            if (t.EntityID == this.EntityID &&
+                t.ComponentType == this.ComponentType &&
+                t.ComponentName == this.ComponentName &&
+                t.Data == this.Data &&
+                t.DataIsString == this.DataIsString)
+                return true;
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 13;
+            hash += this.EntityID.GetHashCode();
+            hash += (null == this.ComponentType ? 0 : this.ComponentType.GetHashCode());
+            hash += (null == this.ComponentName ? 0 : this.ComponentName.GetHashCode());
+            hash += (null == this.Data ? 0 : this.Data.GetHashCode());
+            hash += this.DataIsString.GetHashCode();
+            return hash;
+        }
+    }
+}
